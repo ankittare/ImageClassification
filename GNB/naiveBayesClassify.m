944aa26c7dfdf8c761c87166b5ef6d472e3d3ca9
bernieyiweizhao <z-yiwei@hotmail.com>
2015-11-30 13:51:19 -0500

    implemented normpdf

--- GNB/naiveBayesClassify.m
+++ GNB/naiveBayesClassify.m
@@ -4,11 +4,12 @@ function [t] = naiveBayesClassify(xTest, M, V, p)
     t = zeros(sample,1);
     for j = 1:sample
         y = log(p);
-        x = xTest(j,:);
+        x = xTest(j,:)';
         for k = 1:class
             mk = M(:,k);
-            vk = V(:,k).^(1/2);
-            y(k) = y(k) + logProd(log(normpdf(x',mk,vk)));    
+            vk = V(:,k);
+            prob = 1./sqrt(2*pi*vk).*exp(-0.5*((x-mk)./vk.^(1/2)).^2);%normpdf
+            y(k) = y(k) + sum(log(prob));    
         end
         i = find(y==max(y));
         t(j) = i(1)-1;
